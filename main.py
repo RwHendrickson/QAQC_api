69fea6323d833a994335e09c1a0c747120ffa427
RwHendrickson <91440903+RwHendrickson@users.noreply.github.com>
2023-05-08 13:09:30 -0500

    Adding RealTime Interpolations to List

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from flask import Flask, request
 ## interpolation names
 
 interp_names = ['PM2_5_Kriging', 'Temp_Kriging', 'elev_kriging', 
-                'PM2_5_Kriging_Errors', 'Temp_Kriging_Errors', 'elev_kriging_errors']
+                'PM2_5_Kriging_Errors', 'Temp_Kriging_Errors', 'elev_kriging_errors', 'PM2_5_All_May8', 'PM2_5_Errors_All_May8',
+                'PM2_5_Spring_May8', 'PM2_5_Errors_Spring_May8',
+                'PM2_5_Summer_May8', 'PM2_5_Errors_Summer_May8']
 
 ## Database Credentials
 
